ae3af926fc1ec335fe9b1a2ab76f53b7a841e4c6
JuanMa <JuanMBriones@outlook.com>
2022-06-11 00:53:22 +0000

    Add Async for fetching JSON data

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,53 +15,42 @@
 /**
  * Adds a random greeting to the page.
  */
-function getExperience() {
-  
-  const greetings = [
-    {
-      'title': 'Data Engineer Intern',
-      'company': 'Neoris',
-      'tasks': [
-        'Designed a CLI tool in Python to help more than 100 ML Engineers to test and deploy models into production.',
-        'Reduced cost of Azure instance by 40% by implementing a Kubernetes Airflow instance and monitoring pod’s status using Graphana.',
-        'Created a Data Warehouse architecture and created ETLs for data transformation with Python.'
-      ],
-      'img': {
-        'path': 'images/neoris_logo.png',
-        'alt': 'Neoris logo'
-      }
-    },
-    {
-      'title': 'Data Engineer Intern',
-      'company': 'Neoris',
-      'tasks': [
-        'Designed a CLI tool in Python to help more than 100 ML Engineers to test and deploy models into production.',
-        'Reduced cost of Azure instance by 40% by implementing a Kubernetes Airflow instance and monitoring pod’s status using Graphana.',
-        'Created a Data Warehouse architecture and created ETLs for data transformation with Python.'
-      ],
-      'img': {
-        'path': 'images/neoris_logo.png',
-        'alt': 'Neoris logo'
-      }
-    },
-    {
-      'title': 'Data Engineer Intern',
-      'company': 'Neoris',
-      'tasks': [
-        'Designed a CLI tool in Python to help more than 100 ML Engineers to test and deploy models into production.',
-        'Reduced cost of Azure instance by 40% by implementing a Kubernetes Airflow instance and monitoring pod’s status using Graphana.',
-        'Created a Data Warehouse architecture and created ETLs for data transformation with Python.'
-      ],
-      'img': {
-        'path': 'images/neoris_logo.png',
-        'alt': 'Neoris logo'
-      }
-    }    
-  ];
-  // Pick a random greeting.
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+
+async function fetchExperienceJSON() {
+  const response = await fetch("./experience.json")
+  const experience = await response.json();
+  return experience;
 }
+
+fetchExperienceJSON().then(experience => {
+    console.log(experience)
+    for (var key in experience) {
+      var title = experience[key].title;
+      var company = experience[key].company;
+      var tasks = [];
+    
+      var list_tasks = document.createElement('ul');
+      list_tasks.class = 'list';
+      list_tasks.innerHTML = '';
+      for(var task in experience[key].tasks) {
+        tasks.push(experience[key].tasks[task]);
+        list_tasks.innerHTML += '<li>' + experience[key].tasks[task] + '</li>';
+      }
+      var img_path = experience[key].img.path
+      var img_alt = experience[key].img.alt
+    
+      var badge = document.createElement('div');
+      badge.className = 'badge';
+      badge.innerHTML = 
+        '<h2>' + title + '</h2>' +
+        '<h3>' + company + '</h3>' +
+        list_tasks.innerHTML +
+        '<img src=\"'+ img_path +'\" alt=\"'+ img_alt +'\">'
+      document.getElementById('experience').appendChild(badge);
+    }
+});
+
+
+
 
